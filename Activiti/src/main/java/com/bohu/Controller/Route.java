9d89515ac6c18972e08166801431717278ba15dd
bohu <shenbohu@gmail.com>
2022-06-30 16:33:58 +0800

    sa token

--- Activiti/src/main/java/com/bohu/Controller/Route.java
+++ Activiti/src/main/java/com/bohu/Controller/Route.java
@@ -1,24 +1,35 @@
 package com.bohu.Controller;
 
 import com.bohu.Controller.utils.FlowUtils;
-import org.activiti.bpmn.model.BpmnModel;
-import org.activiti.bpmn.model.FlowNode;
-import org.activiti.bpmn.model.SequenceFlow;
+import com.bohu.Controller.utils.HttpUtils;
+import org.activiti.bpmn.model.*;
+import org.activiti.bpmn.model.Process;
 import org.activiti.engine.HistoryService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricActivityInstance;
 import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntityImpl;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.Model;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.task.Task;
 import org.activiti.image.ProcessDiagramGenerator;
+import org.activiti.image.impl.DefaultProcessDiagramGenerator;
 import org.activiti.spring.ProcessEngineFactoryBean;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.jce.exception.ExtIOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -40,16 +51,19 @@ public class Route {
     @Autowired
     private ProcessEngineFactoryBean processEngine;
 
+    @Autowired
+    private TaskService taskService;
+
+
     /**
      * 查看实例流程图，根据流程实例ID获取流程图
      */
-    @RequestMapping(value="traceprocess/{instanceId}",method=RequestMethod.GET)
-    public void traceprocess(HttpServletResponse response, @PathVariable("instanceId")String instanceId) throws Exception{
+    @RequestMapping(value = "traceprocess/{instanceId}", method = RequestMethod.GET)
+    public void traceprocess(HttpServletResponse response, @PathVariable("instanceId") String instanceId) throws Exception {
         InputStream in = getResourceDiagramInputStream(instanceId);
         ServletOutputStream output = response.getOutputStream();
         IOUtils.copy(in, output);
     }
-
 
 
     /**
@@ -58,7 +72,7 @@ public class Route {
      * @param id
      * @return
      */
-    public  InputStream getResourceDiagramInputStream(String id) {
+    public InputStream getResourceDiagramInputStream(String id) {
         try {
             // 获取历史流程实例
             HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(id).singleResult();
@@ -106,5 +120,68 @@ public class Route {
         return executedFlowIdList;
 
     }
+
+
+    @GetMapping("processDefinitionImage")
+    public void processDefinitionImage(String processDefinitionId, HttpServletResponse response) throws Exception {
+        processDefinitionId = "47501";
+        if (StringUtils.isBlank(processDefinitionId)) {
+            throw new Exception("processDefinitionId 不能为空!");
+        }
+        InputStream inputStream = getDefinitionImage(processDefinitionId);
+        OutputStream outputStream = response.getOutputStream();
+        HttpUtils.copyImageStream(inputStream, outputStream);
+    }
+
+    /**
+     * 获取流程定义图片
+     *
+     * @param processDefinitionId
+     * @return
+     */
+    public InputStream getDefinitionImage(String processDefinitionId) {
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().processDefinitionId(processDefinitionId).singleResult();
+        InputStream inputStream = repositoryService.getResourceAsStream(processDefinition.getDeploymentId(), processDefinition.getDiagramResourceName());
+        return inputStream;
+    }
+
+
+    @GetMapping("ProcessInstanceImage/{deploymentId}/{type}")
+    public void currentProcessInstanceImage(@PathVariable("deploymentId") String deploymentId,
+                                            @PathVariable("type") boolean type,
+                                            HttpServletResponse response) throws IOException {
+        List<String> names = repositoryService.getDeploymentResourceNames(deploymentId);
+        String imageName = "";
+        for (String name : names) {
+            if (name.indexOf(".png") >= 0) {
+                imageName = name;
+            }
+        }
+        if (imageName != "") {
+            File file = new File("/Users/shenbohu/Downloads/" + imageName);
+            InputStream fis = repositoryService.getResourceAsStream(deploymentId, imageName);
+            response.reset(); // 非常重要
+            if (type) {
+                //response.setContentType(u.openConnection().getContentType());
+                response.setHeader("Content-Disposition", "inline; filename=" + imageName);
+            } else {
+                response.setContentType("application/x-msdownload");
+                response.setHeader("Content-Disposition", "attachment; filename=" + imageName);
+            }
+            OutputStream outStream = response.getOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                outStream.write(buffer, 0, len);
+            }
+            fis.close();
+            outStream.close();
+//            FileUtils.copyInputStreamToFile(repositoryService.getResourceAsStream(id, imageName), file);
+//            file.createNewFile();  下载到本地 file
+        }
+
+    }
+
 
 }
